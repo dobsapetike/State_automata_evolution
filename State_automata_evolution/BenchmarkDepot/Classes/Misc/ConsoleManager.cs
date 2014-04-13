035cbcb6b46d8744c92e004175b61fb5557e3a71
dobsapetike <dobsa.petike@gmail.com>
2014-04-13 23:48:59 +0200

    testable experiments in console

---
+++ State_automata_evolution/BenchmarkDepot/Classes/Misc/ConsoleManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BenchmarkDepot.Classes.Misc
+{
+
+    /// <summary>
+    /// Static class for providing console window commands
+    /// </summary>
+    public static class ConsoleManager
+    {
+
+        #region Console commands
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 5;
+
+        [DllImport("kernel32")]
+        public static extern bool AllocConsole();
+
+        [DllImport("kernel32")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32")]
+        private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+
+        public static void ShowConsole()
+        {
+            ShowWindow(GetConsoleWindow(), SW_SHOW);
+        }
+
+        public static void HideConsole()
+        {
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
+        }
+
+        #endregion
+
+    }
+
+}
